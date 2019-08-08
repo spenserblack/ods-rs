40358efefb45ade34229fe2104cb5063bffc9ff7
Spenser Black <spenserblack01@gmail.com>
2019-08-08 13:23:51 -0400

    Document uses for Die

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,50 @@ use rand::Rng;
 
 /// Represents a single die.
 /// Has an initial random value.
+///
+/// # Examples
+/// ## Adding roll values
+/// ```
+/// use one_d_six::Die;
+///
+/// let mut d4 = Die::new(4);
+/// let mut d6 = Die::new(6);
+///
+/// let d4_result: u32 = d4.roll();
+/// let d6_result: u32 = d6.roll();
+/// let result = d4_result + d6_result;
+///
+/// assert!(result >= 2);
+/// assert!(result <= 10);
+/// ```
+///
+/// ## Adding current values
+/// ```
+/// use one_d_six::Die;
+///
+/// let d4 = Die::new(4);
+/// let d6 = Die::new(6);
+///
+/// let d4_result: u32 = d4.current_face();
+/// let d6_result: u32 = d6.current_face();
+/// let result = d4_result + d6_result;
+///
+/// assert!(result >= 2);
+/// assert!(result <= 10);
+/// ```
+///
+/// ## Adding dice directly
+/// ```
+/// use one_d_six::Die;
+///
+/// let d4 = Die::new(4);
+/// let d6 = Die::new(6);
+///
+/// let result = d4 + d6;
+///
+/// assert!(result >= 2);
+/// assert!(result <= 10);
+/// ```
 pub struct Die {
     faces: u32,
     current_value: u32,
