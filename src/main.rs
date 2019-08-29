2d5bc3e8c91b31796e3f0e652a945f60b59ad9e4
Spenser Black <spenserblack01@gmail.com>
2019-08-29 12:51:05 -0400

    Correct version number

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
         .long("complex")
         .help("If you want each cast die to be printed");
     let app_args = App::new("One D Six")
-        .version("0.4.0")
+        .version("0.5.0")
         .about("Rolls some dice")
         .arg(input_arg)
         .arg(complexity_arg);
